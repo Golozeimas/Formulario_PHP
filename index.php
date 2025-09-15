a20f25f993bad93c78d43969612ee0ba82d6e1e5
Golozeimas <joaosobramatheus@gmail.com>
2025-09-15 16:44:01 -0300

    modificando o HTML do forms

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,8 +10,17 @@
     <h1>Formulário com PHP</h1>
     <h3 style="color:red;">*Obrigatório</h3>
     <form action="" method="POST">
-    <label for="nome"> Nome : </label> <input type="text" id="name" required>
-    <label for=""></label>
+    <label for="nome"> Nome : </label> <input type="text" id="name" required> <br> <br>
+    <label for="email">E-mail:</label> <input type="text" id="email" required> <br><br>
+    <label for="website">Website:</label> <input type="text" id="website" required> <br> <br>
+    <label for="comentario" style="display: block;">Comentário:</label> <textarea id="comentario" cols="30" rows="10"> </textarea> 
+    <br> <br>
+    <label for="genero">Gênero:</label> <input type="radio" value="Masculino" name="genero">Masculino
+                                        <input type="radio" value="Feminino" name="genero">Feminino
+                                        <br><br>
+                                        <button id="enviado" type="submit">Enviar</button>
     </form>
+
+    
 </body>
 </html>
