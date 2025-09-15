223217fef3d498d418b6491e203de4ec3a9c0dec
Golozeimas <joaosobramatheus@gmail.com>
2025-09-15 16:08:26 -0300

    inicializando repositório de um simples formulário com PHP, para testes em banco de dados

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaŕio</title>
+</head>
+<body>
+    
+    <h1>Formulário com PHP</h1>
+    <h3 style="color:red;">*Obrigatório</h3>
+    <form action="" method="POST">
+    <label for="nome"> Nome : </label> <input type="text" id="name" required>
+    <label for=""></label>
+    </form>
+</body>
+</html>
